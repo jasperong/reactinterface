5e0bc3d136ffe50ec037a3aeaf9cffdc2f3f56a0
Jasper <jave.ong4@yahoo.com>
2016-11-23 19:45:40 -0500

    finish section 9

--- process/js/app.js
+++ process/js/app.js
@@ -3,13 +3,17 @@ var ReactDOM = require('react-dom');
 var _ = require('lodash');
 
 var AptList = require('./AptList');
-var AddAppointment = require('./AddAppointment')
+var AddAppointment = require('./AddAppointment');
+var SearchAppointments = require('./SearchAppointments');
 
 var MainInterface = React.createClass({
   getInitialState: function() {
     return {
       myAppointments: [],
-      aptBodyVisible: false
+      aptBodyVisible: false,
+      orderBy: 'petName',
+      orderDir: 'asc',
+      queryText: ''
     }
   },
 
@@ -45,8 +49,39 @@ var MainInterface = React.createClass({
     this.setState({myAppointments: tempApts})
   },
 
+  reOrder: function(orderBy, orderDir) {
+    this.setState({
+      orderBy: orderBy,
+      orderDir: orderDir
+    });
+  },
+
+  searchApts: function(q) {
+    this.setState({queryText: q})
+  },
+
   render: function() {
-    var filteredApts = this.state.myAppointments;
+    var filteredApts = [];
+    var orderBy = this.state.orderBy;
+    var orderDir = this.state.orderDir;
+    var queryText = this.state.queryText;
+    var myAppointments = this.state.myAppointments;
+
+    myAppointments.forEach(function(item) {
+      if(
+        (item.petName.toLowerCase().indexOf(queryText) != -1) ||
+        (item.ownerName.toLowerCase().indexOf(queryText) != -1) ||
+        (item.aptDate.toLowerCase().indexOf(queryText) != -1) ||
+        (item.aptNotes.toLowerCase().indexOf(queryText) != -1)
+      ) {
+        filteredApts.push(item);
+      }
+    })
+
+    filteredApts = _.orderBy(filteredApts, function(item) {
+      return item[orderBy].toLowerCase();
+    }, orderDir);
+
 
     filteredApts = filteredApts.map(function(item, i) {
       return (
@@ -62,6 +97,10 @@ var MainInterface = React.createClass({
         <AddAppointment bodyVisible={this.state.aptBodyVisible}
                         handleToggle={this.toggleAddDisplay}
                         addApt={this.addItem} />
+        <SearchAppointments orderBy={this.state.orderBy}
+                            orderDir={this.state.orderDir}
+                            onReOrder={this.reOrder}
+                            onSearch={this.searchApts} />
         <ul className='item-list media-list'>
           {filteredApts}
         </ul>
